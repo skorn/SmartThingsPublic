ac683e4ef48717231ef23de102e4c68de9b90b4e
Martin Lariz <mlariz@gmail.com>
2016-04-04 16:45:22 -0700

    Committing many changes to try to get a working alpha test.

--- smartapps/skorn/kid-escape-alarm.src/kid-escape-alarm.groovy
+++ smartapps/skorn/kid-escape-alarm.src/kid-escape-alarm.groovy
@@ -15,7 +15,7 @@
  *  Author: Martin Lariz
  */
 definition(
-    name: "Kid escape alarm",
+    name: "Kid escaping alarm",
     namespace: "skorn",
     author: "Martin Lariz",
     description: "Use combination of siren, door sensor and switch to detect child leaving through frontdoor unattended.",
@@ -25,33 +25,35 @@ definition(
 )
 
 preferences {
-	section("Door sensor to monitor.") {
-		input "door", "capability.contactSensor", required: true
-	}
-	section("Switch used to disable alarm.") {
-		input "master", "capability.switch", required: true
-	}
-	section("Siren to alarm with.") {
-		input "siren", "capability.alarm", required: true
-	}
-	section("Optional second button to disable alarm") {
-		input "secondary", "capability.button", required: false
-	}
+    section("Door sensor to monitor.") {
+        input "door", "capability.contactSensor", required: true
+    }
+    section("Siren to alarm with.") {
+        input "siren", "capability.alarm", required: true
+        input "sirenTrack", "number", Title: "Which track to alarm with?", required: true
+        input "sirenRepeat", "number", Title: "How often (in seconds) to repeat (should be duration of the alarm to keep constant)?", required: true
+    }
+    section("Switch used to disable alarm.") {
+        input "master", "capability.switch", required: true
+        input "switchSleep", "number", Title: "Sleep for how many seconds?", required: true
+        input "switchInterval", "number", Title: "How many seconds to allow between switch presses?", required: true
+    }
+    section("Optional second button to disable alarm") {
+        input "secondary", "capability.button", required: false
+        input "buttonSleep", "number", Title: "Sleep for how many seconds?", required: true
+    }
 }
 
-def installed()
-{
-	initialize()
+def installed() {
+    initialize()
 }
 
-def updated()
-{
-	unsubscribe()
-	initialize()
+def updated() {
+    unsubscribe()
+    initialize()
 }
 
-def initialize()
-{
+def initialize() {
     subscribe(door, "contact", doorHandler)
     subscribe(master, "switch", switchHandler, [filterEvents: false])
     subscribe(secondary, "button", buttonHandler, [filterEvents: false])
@@ -64,72 +66,106 @@ def doorHandler(evt) {
         if (state.enabled == 1) {
             keepAlarming()
         }
+    } else if (evt.value == "closed") {
+        siren.stop()
     }
 }
 
 def keepAlarming() {
     def currentValue = door.currentValue("contact")
     if (currentValue == "open" && state.enabled == 1) {
-        siren.playTrack(6)
-        runIn(60, keepAlarming)
+        siren.playTrack(sirenTrack)
+        runIn(sirenRepeat, keepAlarming)
     }
 }
 
 def switchHandler(evt) {
-    log.debug now()
-	log.info evt.value
+    // TBD remove next 5 lines
+    currentTime = now()
+    log.debug "Current time: ${currentTime}"
+    log.debug "Switch Name: ${evt.name}"
+    log.debug "Switch Value: ${evt.value}"
+    log.debug "Switch Physical: ${evt.physical}"
 
-	if (evt.physical) {
-        // Allow only up to 3 seconds in between presses, else reset
-		if (state.switchTS > (now() - 3000)) {
-            // Expect up, up, down, down: reset if doesn't matchand if it does sleep alarm 60 seconds
+    if (evt.physical) {
+        if (state.switchTS > ($currentTime - (switchInterval * 1000))) {
+            // Expect up, up, down, down: reset if doesn't matchand if it does sleep alarm
             switch (state.switchHistory) {
                 case 0:
                     if (evt.value == "on") {
+                        log.debug "Sequence: Got first in sequence 'UP, up, down, down'"
                         state.switchHistory = 1
                     } else {
+                        log.debug "Sequence: Isolated off, sequence reset"
                         state.switchHistory = 0
                     }
                     break
                 case 1:
                     if (evt.value == "on") {
+                        log.debug "Sequence: Got second in sequence 'UP, UP, down, down'"
                         state.switchHistory = 2
                     } else {
+                        log.debug "Sequence: Second in sequence incorrect, sequence reset"
                         state.switchHistory = 0
                     }
                     break
                 case 2:
                     if (evt.value == "off") {
+                        log.debug "Sequence: Got third in sequence 'UP, UP, DOWN, down'"
                         state.switchHistory = 3
                     } else {
+                        log.debug "Sequence: Third in sequence incorrect, sequence reset"
                         state.switchHistory = 0
                     }
                     break
                 case 3:
                     if (evt.value == "off") {
-                        sleepAlarm(60)
+                        log.debug "Sequence: Got full sequence 'UP, UP, DOWN, DOWN'"
+                        sleepAlarm(switchSleep)
+                    } else {
+                        log.debug "Sequence: Final down in sequence incorrect, sequence reset"
                     }
                     state.switchHistory = 0
                     break
                 default:
+                    log.debug "Sequence: Got UNEXPECTED switchHistory, resetting sequence"
                     state.switchHistory = 0
                     break
             }
-		} else {
+        } else {
             if (evt.value == "on" ) {
+                log.debug "Sequence: Got first in sequence 'UP, up, down, down'"
                 state.switchHistory = 1
             } else {
+                log.debug "Sequence: Isolated off, sequence reset"
                 state.switchHistory = 0
             }
         }
         state.switchTS = now()
-	} else {
-		log.trace "Skipping digital on/off event"
-	}
+    } else {
+        log.trace "Skipping digital on/off event"
+    }
+}
+
+def buttonHandler(evt) {
+    // TBD remove next 5 lines
+    currentTime = now()
+    log.debug "Current time: ${currentTime}"
+    log.debug "Button Name: ${evt.name}"
+    log.debug "Button Value: ${evt.value}"
+    log.debug "Button Physical: ${evt.physical}"
+
+    if (evt.physical) {
+        // TBD Ensure button press is captured properly, may need an "if" statement here
+        sleepAlarm(buttonSleep)
+    } else {
+        log.trace "Skipping digital on/off event"
+    }
 }
 
 def sleepAlarm(sleepTime) {
     state.enabled = 0
+    siren.stop()
     runIn(sleepTime, enableAlarm)
 }
 
